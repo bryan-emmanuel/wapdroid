cdb6b19d283ba7bf22c29124c77f1c412a0cfd5c
Bryan Emmanuel <piusvelte@gmail.com>
2010-07-14 18:50:58 +0000

    auto-restart-onupgrade

--- src/com/piusvelte/wapdroid/BootReceiver.java
+++ src/com/piusvelte/wapdroid/BootReceiver.java
@@ -26,9 +26,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 
 public class BootReceiver extends BroadcastReceiver {
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+		// on boot, or package upgrade, start the service
+		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(Intent.ACTION_PACKAGE_REPLACED)) {
 			SharedPreferences sp = context.getSharedPreferences(context.getString(R.string.key_preferences), WapdroidService.MODE_PRIVATE);
 			if (sp.getBoolean(context.getString(R.string.key_manageWifi), true)) {
 				ManageWakeLocks.acquire(context);
