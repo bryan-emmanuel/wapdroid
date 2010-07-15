79f509ae0f8755aaa0ff7bce757bb0403a93d817
Bryan Emmanuel <piusvelte@gmail.com>
2010-07-15 02:13:31 +0000

    adding-backupagent

--- src/com/piusvelte/wapdroid/DatabaseHelper.java
+++ src/com/piusvelte/wapdroid/DatabaseHelper.java
@@ -1,3 +1,23 @@
+/*
+ * Wapdroid - Android Location based Wifi Manager
+ * Copyright (C) 2009 Bryan Emmanuel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Bryan Emmanuel piusvelte@gmail.com
+ */
+
 package com.piusvelte.wapdroid;
 
 import static com.piusvelte.wapdroid.WapdroidDbAdapter.CREATE_CELLS;
@@ -26,7 +46,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-	private static final String DATABASE_NAME = "wapdroid";
+	public static final String DATABASE_NAME = "wapdroid";
 	private static final int DATABASE_VERSION = 4;
 	private static final String DROP = "drop table if exists ";
 
