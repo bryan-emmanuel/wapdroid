ed5c84e95e7d3ea09a5d9630714e18a8f75c4683
Bryan Emmanuel <piusvelte@gmail.com>
2010-07-19 03:10:56 +0000

    ui-updt

---
+++ src/com/piusvelte/wapdroid/BackupAgentWrapper.java
@@ -0,0 +1,63 @@
+/*
+ * Wapdroid - Android Location based Wifi Manager
+ * Copyright (C) 2009 Bryan Emmanuel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Bryan Emmanuel piusvelte@gmail.com
+ */
+
+package com.piusvelte.wapdroid;
+
+import java.io.IOException;
+
+import android.app.backup.BackupDataInput;
+import android.app.backup.BackupDataOutput;
+import android.os.ParcelFileDescriptor;
+
+public class BackupAgentWrapper {
+	private WapdroidBackupAgent wba;
+	static {
+		try {
+			Class.forName("WapdroidBackupAgent");
+		}
+		catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public static void checkAvailable() {}
+
+	public BackupAgentWrapper() {
+		wba = new WapdroidBackupAgent();
+	}
+
+	public void onCreate() {
+		wba.onCreate();
+	}
+
+	public void onDestroy() {
+		wba.onDestroy();
+	}
+
+	public void onBackup(ParcelFileDescriptor oldState, BackupDataOutput data, ParcelFileDescriptor newState) throws IOException {
+		wba.onBackup(oldState, data, newState);
+
+	}
+
+	public void onRestore(BackupDataInput data, int appVersionCode, ParcelFileDescriptor newState) throws IOException {
+		wba.onRestore(data, appVersionCode, newState);
+
+	}
+}
