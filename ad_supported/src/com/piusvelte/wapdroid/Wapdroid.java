3c5eb011ae0cc321b9f1deb9a92c160c8d8d6310
piusvelte <piusvelte@gmail.com>
2012-05-31 11:22:12 -0400

    rebuild repository structure

---
+++ ad_supported/src/com/piusvelte/wapdroid/Wapdroid.java
@@ -0,0 +1,108 @@
+/*
+ * Wapdroid - Android Location based Wifi Manager
+ * Copyright (C) 2009 Bryan Emmanuel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Bryan Emmanuel piusvelte@gmail.com
+ */
+package com.piusvelte.wapdroid;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class Wapdroid {
+
+	public static final int UNKNOWN_CID = -1;
+	public static final int UNKNOWN_RSSI = 99;
+	
+	public static final String ACTION_TOGGLE_SERVICE = "com.piusvelte.wapdroid.Wapdroid.TOGGLE_SERVICE";
+	protected static final String GOOGLE_AD_ID = "a14c03f0ced257b";
+	protected static final String PRO = "pro";
+
+	private Wapdroid() {}
+
+	public static final class Networks implements BaseColumns {
+		private Networks() {}
+    	
+    	public static final Uri CONTENT_URI = Uri.parse("content://"
+    			+ WapdroidProvider.AUTHORITY + "/networks");
+    	
+    	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.piusvelte.networks";
+    	
+    	public static final String SSID = "ssid";
+    	public static final String BSSID = "bssid";
+    	public static final String MANAGE = "manage";
+	}
+	
+	public static final class Cells implements BaseColumns {
+		private Cells() {}
+    	
+    	public static final Uri CONTENT_URI = Uri.parse("content://"
+    			+ WapdroidProvider.AUTHORITY + "/cells");
+    	
+    	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.piusvelte.cells";
+    	
+    	public static final String CID = "cid";
+    	public static final String LOCATION = "location";
+	}
+	
+	public static final class Locations implements BaseColumns {
+		private Locations() {}
+    	
+    	public static final Uri CONTENT_URI = Uri.parse("content://"
+    			+ WapdroidProvider.AUTHORITY + "/locations");
+    	
+    	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.piusvelte.locations";
+    	
+    	public static final String LAC = "lac";
+	}
+	
+	public static final class Pairs implements BaseColumns {
+		private Pairs() {}
+    	
+    	public static final Uri CONTENT_URI = Uri.parse("content://"
+    			+ WapdroidProvider.AUTHORITY + "/pairs");
+    	
+    	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.piusvelte.pairs";
+    	
+    	public static final String CELL = "cell";
+    	public static final String NETWORK = "network";
+    	public static final String RSSI_MIN = "rssi_min";
+    	public static final String RSSI_MAX = "rssi_max";
+    	public static final String MANAGE_CELL = "manage_cell";
+	}
+	
+	public static final class Ranges implements BaseColumns {
+		private Ranges() {}
+    	
+    	public static final Uri CONTENT_URI = Uri.parse("content://"
+    			+ WapdroidProvider.AUTHORITY + "/ranges");
+    	
+    	public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.piusvelte.ranges";
+    	
+    	public static final String RSSI_MIN = "rssi_min";
+    	public static final String RSSI_MAX = "rssi_max";
+    	public static final String CID = "cid";
+    	public static final String LAC = "lac";
+    	public static final String LOCATION = "location";
+    	public static final String SSID = "ssid";
+    	public static final String BSSID = "bssid";
+    	public static final String CELL = "cell";
+    	public static final String NETWORK = "network";
+    	public static final String MANAGE = "manage";
+    	public static final String MANAGE_CELL = "manage_cell";
+	}
+
+}
---
+++ pro/gen/com/piusvelte/wapdroidpro/IWapdroidUI.java
@@ -0,0 +1,230 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ * Original file: /home/bemmanuel/Documents/development/android/wapdroid/src/com/piusvelte/wapdroidpro/IWapdroidUI.aidl
+ */
+package com.piusvelte.wapdroidpro;
+public interface IWapdroidUI extends android.os.IInterface
+{
+/** Local-side IPC implementation stub class. */
+public static abstract class Stub extends android.os.Binder implements com.piusvelte.wapdroidpro.IWapdroidUI
+{
+private static final java.lang.String DESCRIPTOR = "com.piusvelte.wapdroidpro.IWapdroidUI";
+/** Construct the stub at attach it to the interface. */
+public Stub()
+{
+this.attachInterface(this, DESCRIPTOR);
+}
+/**
+ * Cast an IBinder object into an com.piusvelte.wapdroidpro.IWapdroidUI interface,
+ * generating a proxy if needed.
+ */
+public static com.piusvelte.wapdroidpro.IWapdroidUI asInterface(android.os.IBinder obj)
+{
+if ((obj==null)) {
+return null;
+}
+android.os.IInterface iin = (android.os.IInterface)obj.queryLocalInterface(DESCRIPTOR);
+if (((iin!=null)&&(iin instanceof com.piusvelte.wapdroidpro.IWapdroidUI))) {
+return ((com.piusvelte.wapdroidpro.IWapdroidUI)iin);
+}
+return new com.piusvelte.wapdroidpro.IWapdroidUI.Stub.Proxy(obj);
+}
+public android.os.IBinder asBinder()
+{
+return this;
+}
+@Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+{
+switch (code)
+{
+case INTERFACE_TRANSACTION:
+{
+reply.writeString(DESCRIPTOR);
+return true;
+}
+case TRANSACTION_setOperator:
+{
+data.enforceInterface(DESCRIPTOR);
+java.lang.String _arg0;
+_arg0 = data.readString();
+this.setOperator(_arg0);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_setCellInfo:
+{
+data.enforceInterface(DESCRIPTOR);
+int _arg0;
+_arg0 = data.readInt();
+int _arg1;
+_arg1 = data.readInt();
+this.setCellInfo(_arg0, _arg1);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_setCells:
+{
+data.enforceInterface(DESCRIPTOR);
+java.lang.String _arg0;
+_arg0 = data.readString();
+this.setCells(_arg0);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_setBattery:
+{
+data.enforceInterface(DESCRIPTOR);
+int _arg0;
+_arg0 = data.readInt();
+this.setBattery(_arg0);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_setWifiInfo:
+{
+data.enforceInterface(DESCRIPTOR);
+int _arg0;
+_arg0 = data.readInt();
+java.lang.String _arg1;
+_arg1 = data.readString();
+java.lang.String _arg2;
+_arg2 = data.readString();
+this.setWifiInfo(_arg0, _arg1, _arg2);
+reply.writeNoException();
+return true;
+}
+case TRANSACTION_setSignalStrength:
+{
+data.enforceInterface(DESCRIPTOR);
+int _arg0;
+_arg0 = data.readInt();
+this.setSignalStrength(_arg0);
+reply.writeNoException();
+return true;
+}
+}
+return super.onTransact(code, data, reply, flags);
+}
+private static class Proxy implements com.piusvelte.wapdroidpro.IWapdroidUI
+{
+private android.os.IBinder mRemote;
+Proxy(android.os.IBinder remote)
+{
+mRemote = remote;
+}
+public android.os.IBinder asBinder()
+{
+return mRemote;
+}
+public java.lang.String getInterfaceDescriptor()
+{
+return DESCRIPTOR;
+}
+public void setOperator(java.lang.String operator) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeString(operator);
+mRemote.transact(Stub.TRANSACTION_setOperator, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+public void setCellInfo(int cid, int lac) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeInt(cid);
+_data.writeInt(lac);
+mRemote.transact(Stub.TRANSACTION_setCellInfo, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+public void setCells(java.lang.String cells) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeString(cells);
+mRemote.transact(Stub.TRANSACTION_setCells, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+public void setBattery(int batteryPercentage) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeInt(batteryPercentage);
+mRemote.transact(Stub.TRANSACTION_setBattery, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+public void setWifiInfo(int state, java.lang.String ssid, java.lang.String bssid) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeInt(state);
+_data.writeString(ssid);
+_data.writeString(bssid);
+mRemote.transact(Stub.TRANSACTION_setWifiInfo, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+public void setSignalStrength(int rssi) throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+_data.writeInt(rssi);
+mRemote.transact(Stub.TRANSACTION_setSignalStrength, _data, _reply, 0);
+_reply.readException();
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+}
+}
+static final int TRANSACTION_setOperator = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+static final int TRANSACTION_setCellInfo = (android.os.IBinder.FIRST_CALL_TRANSACTION + 1);
+static final int TRANSACTION_setCells = (android.os.IBinder.FIRST_CALL_TRANSACTION + 2);
+static final int TRANSACTION_setBattery = (android.os.IBinder.FIRST_CALL_TRANSACTION + 3);
+static final int TRANSACTION_setWifiInfo = (android.os.IBinder.FIRST_CALL_TRANSACTION + 4);
+static final int TRANSACTION_setSignalStrength = (android.os.IBinder.FIRST_CALL_TRANSACTION + 5);
+}
+public void setOperator(java.lang.String operator) throws android.os.RemoteException;
+public void setCellInfo(int cid, int lac) throws android.os.RemoteException;
+public void setCells(java.lang.String cells) throws android.os.RemoteException;
+public void setBattery(int batteryPercentage) throws android.os.RemoteException;
+public void setWifiInfo(int state, java.lang.String ssid, java.lang.String bssid) throws android.os.RemoteException;
+public void setSignalStrength(int rssi) throws android.os.RemoteException;
+}
